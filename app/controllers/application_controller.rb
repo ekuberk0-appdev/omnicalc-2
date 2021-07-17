3e63fabd29407deeddb9ea225fcb54f0c82d4f65
edita21 <ekuberk0@chicagobooth.edu>
2021-07-17 15:48:26 +0000

    4% commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
 
+def addition_results
+  render("add_templates/results.html.erb")
+end 
+
+def add_form
+  render("add_templates/addform.html.erb")
+end 
+
+def subtract_form
+  render("subtract_templates/subtract_form.html.erb")
+end 
+
+def subtract_results
+render("subtract_templates/subtract_results.html.erb")
+end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
 
+  get("/wizard_add", { :controller => "application", :action => "addition_results" })
+
+  get("/add", { :controller => "application", :action => "add_form"})
+
+  get("/wizard_subtract", { :controller => "application", :action => "subtract_results"})
+
+  get("/subtract", { :controller => "application", :action => "subtract_form"})
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
